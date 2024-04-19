3d867335c842d175064996252e247e6b2946c650
Zattox <vladcsgoogurechik@mail.ru>
2024-04-19 17:44:23 +0300

    Добавил папку для Занятия 4

---
+++ Algorithms_1.0/Lesson_4/E.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef long double ld;
+using std::cin, std::cout;
+
+void solve() {
+
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solve();
+
+  return 0;
+}
