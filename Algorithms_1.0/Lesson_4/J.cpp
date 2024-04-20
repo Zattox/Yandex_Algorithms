e5306d25d3c3d6249ac11b3fe707ccd533158fc4
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-04-20 16:09:01 +0300

    Сделал задачу J

--- Algorithms_1.0/Lesson_4/J.cpp
+++ Algorithms_1.0/Lesson_4/J.cpp
@@ -2,9 +2,78 @@
 
 typedef long long ll;
 typedef long double ld;
-using std::cin, std::cout;
+using namespace std;
+
+string to_lower(string str) {
+  string res;
+  for (int i = 0; i < str.size(); ++i) {
+    res += char(tolower(str[i]));
+  }
+  return res;
+}
 
 void solve() {
+  int n;
+  string c, d;
+  cin >> n >> c >> d;
+
+  set<string> dict;
+  for (int i = 0; i < n; ++i) {
+    string word;
+    cin >> word;
+    if (c == "no") {
+      word = to_lower(word);
+    }
+    dict.insert(word);
+  }
+
+  int ch = 0;
+  map<string, int> indx;
+  map<string, int> mp;
+  string line;
+  while (getline(cin, line)) {
+    int i = 0;
+    while (i < line.size()) {
+      string word;
+      while (i < line.size() && (line[i] == '_' || (!ispunct(line[i]) && !isspace(line[i])))) {
+        word += line[i];
+        ++i;
+      }
+      if (!word.empty()) {
+        if (c == "no") {
+          word = to_lower(word);
+        }
+        if (d == "yes" || !isdigit(word[0])) {
+          if (dict.count(word) == 0 && !(word.size() == 1 && isdigit(word[0]))) {
+            if (mp[word] == 0) {
+              indx[word] = ch;
+              ++ch;
+            }
+            mp[word]++;
+          }
+        }
+      }
+      ++i;
+    }
+  }
+
+  int maxi = -1;
+  string ans = "-1";
+  for (auto el : mp) {
+    if (el.second > maxi) {
+      maxi = el.second;
+      ans = el.first;
+    }
+    if (el.second == maxi && indx[el.first] < indx[ans]) {
+      ans = el.first;
+    }
+  }
+
+  if (ans == "-1") {
+    exit(1230123);
+  }
+
+  cout << ans;
 
 }
 
@@ -12,6 +81,9 @@ int main() {
   std::ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
+
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
 
   solve();
 
