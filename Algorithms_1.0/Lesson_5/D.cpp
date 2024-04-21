5485925f688e8ea3e5f96e31cfbae3a3d956797e
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-04-21 20:30:12 +0300

    Добавил папку для 5 занятия

---
+++ Algorithms_1.0/Lesson_5/D.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  while (t > 0) {
+    solve();
+    --t;
+  }
+
+  return 0;
+}
