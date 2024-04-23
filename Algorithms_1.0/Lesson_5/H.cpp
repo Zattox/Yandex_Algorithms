089640fa6b6fc6267498cee54e0bae00a9cfc001
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-04-24 01:45:15 +0300

    Сдал H

--- Algorithms_1.0/Lesson_5/H.cpp
+++ Algorithms_1.0/Lesson_5/H.cpp
@@ -5,7 +5,40 @@ typedef long double ld;
 using namespace std;
 
 void solve() {
+  int n, k;
+  string str;
+  cin >> n >> k >> str;
 
+  int maxi = 1, l = 0, r = 0;
+  int ind = 0;
+  unordered_map<char, int> cnt;
+  ++cnt[str[r]];
+  while (r + 1 < n) {
+    ++r;
+    if (cnt[str[r]] + 1 <= k) {
+      ++cnt[str[r]];
+      if (maxi < r - l + 1) {
+        maxi = r - l + 1;
+        ind = l;
+      }
+    } else {
+      while (cnt[str[r]] >= k) {
+        --cnt[str[l]];
+        ++l;
+      }
+      ++cnt[str[r]];
+      if (maxi < r - l + 1) {
+        maxi = r - l + 1;
+        ind = l;
+      }
+    }
+  }
+  if (maxi < r - l + 1) {
+    maxi = r - l + 1;
+    ind = l;
+  }
+
+  cout << maxi << ' ' << ind + 1;
 }
 
 int main() {
@@ -13,8 +46,8 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  //freopen("input.txt", "r", stdin);
-  //freopen("output.txt", "w", stdout);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
 
   int t = 1;
   //cin >> t;
